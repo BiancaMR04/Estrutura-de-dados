009fab255464ba16170cba7501e2f6472ef05cd5
Bianca Maia Ribeiro <106209010+BiancaMR04@users.noreply.github.com>
2023-03-29 16:39:05 -0300

    formação da classe, atributos, getters e setters

--- Exercício1/PontoCartesiano.java
+++ Exercício1/PontoCartesiano.java
@@ -1,1 +1,27 @@
+public class PontoCartesiano{
 
+    private float x;
+    private float y;
+
+    public PontoCartesiano(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX(){
+        return x;
+    }
+
+    public float getY(){
+        return y;
+    }
+
+    public void setX(float x){
+        this.x = x;
+    }
+
+    public void setY(float y){
+        this.y = y;
+    }
+    
+}
