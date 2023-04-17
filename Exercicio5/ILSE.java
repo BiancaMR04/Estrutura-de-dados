f1f8d9e21f6c5d0d4d4007f546c26d1a7d4677da
Bianca Maia Ribeiro <106209010+BiancaMR04@users.noreply.github.com>
2023-04-17 04:52:19 +0000

    classes ILSE, LSE e Noh com int

---
+++ Exercicio5/ILSE.java
@@ -0,0 +1,10 @@
+package Exercicio5;
+
+public interface ILSE {
+
+    public void insereInicio(int info);
+    public void insereFim(int info);
+    public boolean estahVazia();
+    public boolean remove(int info);
+    public int tamanho();
+}
