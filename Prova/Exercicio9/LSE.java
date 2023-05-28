72a3ec881f8ebb1bf4c25a0c8117572e551fd329
Bianca Maia Ribeiro <106209010+BiancaMR04@users.noreply.github.com>
2023-05-28 06:30:29 +0000

    lse update

--- Prova/Exercicio9/LSE.java
+++ Prova/Exercicio9/LSE.java
@@ -3,8 +3,8 @@ package Prova.Exercicio9;
 public class LSE {
 
     private Noh inicio;
-    
-    public LSE(){
+
+    public LSE() {
         this.inicio = null;
     }
 
@@ -19,61 +19,57 @@ public class LSE {
 
     }
 
-   public void insereFim(int info) {
-    Noh novo = new Noh(info);
+    public void insereFim(int info) {
+        Noh novo = new Noh(info);
         if (inicio == null)
             inicio = novo;
         else {
             Noh ultimo = null;
-                for(Noh i=inicio; i != null; i=i.getProximo())
+            for (Noh i = inicio; i != null; i = i.getProximo())
                 ultimo = i;
-                ultimo.setProximo(novo);
+            ultimo.setProximo(novo);
         }
     }
 
-   public boolean estahVazia() { 
+    public boolean estahVazia() {
         return inicio == null;
-   }
+    }
 
-
-   public boolean remove(int info) {
-    Noh ant=null, p;
-    p = inicio;
-        while (p!=null && p.getInfo() != info){ //busca
+    public boolean remove(int info) {
+        Noh ant = null, p;
+        p = inicio;
+        while (p != null && p.getInfo() != info) { // busca
             ant = p;
             p = p.getProximo();
         }
 
-        if (p==null) // não achou, então não faz nada e retorna false
+        if (p == null) // não achou, então não faz nada e retorna false
             return false;
 
-        if (ant==null) //retira info do início
+        if (ant == null) // retira info do início
             inicio = p.getProximo();
 
         else
             ant.setProximo(p.getProximo());
-            return true;
+        return true;
 
     }
 
+    public int tamanho() {
+        int tamanho = 0;
 
+        for (Noh i = inicio; i != null; i = i.getProximo()) {
+            tamanho++;
+        }
 
-   public int tamanho() {
-    int tamanho = 0;
-
-    for(Noh i = inicio; i != null; i = i.getProximo()) {
-        tamanho++;
+        return tamanho;
     }
-
-    return tamanho;
-    }
-
 
     public String imprimeLista() {
 
         String lista = "";
 
-        for(Noh i = inicio; i != null; i = i.getProximo()) {
+        for (Noh i = inicio; i != null; i = i.getProximo()) {
             lista += i.getInfo() + " ";
         }
 
@@ -91,18 +87,36 @@ public class LSE {
         System.out.print(inicio.getInfo() + " ");
         imprimirRecursivo(inicio.getProximo());
     }
-   
+
     public void bubbleSort() {
 
-        for(Noh i = inicio; i != null; i = i.getProximo()) {
-            for(Noh j = inicio; j != null; j = j.getProximo()) {
-                if( i.getInfo() < j.getInfo()) {
-                    int aux = i.getInfo();
-                    i.setInfo(j.getInfo());
-                    j.setInfo(aux);
+        for (Noh i = inicio; i != null; i = i.getProximo()) {
+            for (Noh j = inicio; j != null; j = j.getProximo()) {
+                if (i.getInfo() < j.getInfo()) {
+                    i.setProximo(j.getProximo());
+                    j.setProximo(i);
+                    if (i == inicio) {
+                        inicio = j;
+                    } else {
+                        Noh anteriorI = getNohAnterior(inicio, i);
+                        anteriorI.setProximo(i.getProximo());
+                    }
+                    Noh anteriorJ = getNohAnterior(inicio, j);
+                    anteriorJ.setProximo(i);
                 }
             }
         }
     }
-    
+
+    private Noh getNohAnterior(Noh inicio, Noh noh) {
+        Noh atual = inicio;
+        while (atual != null) {
+            if (atual.getProximo() == noh) {
+                return atual;
+            }
+            atual = atual.getProximo();
+        }
+        return null;
+    }
+
 }
